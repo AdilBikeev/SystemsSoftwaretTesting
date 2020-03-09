ca81ccc4151c62d2d657747d88b2400c8b776050
AdilBikeev <bikeev1999@mail.ru>
2020-03-09 16:33:29 +0300

    ModuleTest Исправил логику нахождения картинок для приложения

--- ClientChat/MainWindow.xaml.cs
+++ ClientChat/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ClientChat.Controllers;
+using Path = System.IO.Path;
 
 namespace ClientChat
 {
@@ -22,13 +26,36 @@ namespace ClientChat
     /// </summary>
     public partial class MainWindow : Window
     {
+        private enum StatusChat
+        {
+            [Description("Соединение успешно установлено")]
+            Online = 0,
+
+            [Description("Попытка восстановить соединение")]
+            Bad,
+
+            [Description("Соединение разорвано")]
+            Failed
+        }
+
         private MessageController messageController;
+
+        private readonly string currentPath = $"{Directory.GetCurrentDirectory()}/Content/StatusChat";
+
+        private Dictionary<StatusChat, string> statusImagesPath = null;
 
         public MainWindow()
         {
             InitializeComponent();
             messageController = new MessageController();
             _ = messageController.RunPeriodicallyAsync(this.UpdateMsg, new TimeSpan(0, 0, 0, 0, 300), CancellationToken.None);
+
+            statusImagesPath = new Dictionary<StatusChat, string>()
+            {
+                [StatusChat.Online] = Path.Combine(this.currentPath, "online.jpg"),
+                [StatusChat.Bad] = Path.Combine(this.currentPath, "bad.jpg"),
+                [StatusChat.Failed] = Path.Combine(this.currentPath, "failed.jpg"),
+            };
         }
 
         private void SendMsgBtn_Click(object sender, RoutedEventArgs e)
@@ -67,12 +94,12 @@ namespace ClientChat
                     textRange.Text = res;
                     this.chatRtb.ScrollToEnd();
                     this.Title = "Chat - В сети";
-                    this.Icon = new BitmapImage(new Uri(@"./../../../Content/StatusChat/online.jpg", UriKind.RelativeOrAbsolute));
+                    this.Icon = new BitmapImage(new Uri(statusImagesPath[StatusChat.Online], UriKind.RelativeOrAbsolute));
                 }
                 else
                 {
                     this.Title = "Chat - Подключение к сети ...";
-                    this.Icon = new BitmapImage(new Uri(@"./../../../Content/StatusChat/faild.jpg", UriKind.RelativeOrAbsolute));
+                    this.Icon = new BitmapImage(new Uri(statusImagesPath[StatusChat.Failed], UriKind.RelativeOrAbsolute));
                 }
             }
             catch (Exception exc)
